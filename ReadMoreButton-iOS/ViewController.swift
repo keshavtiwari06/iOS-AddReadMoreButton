8fdf6a3a90d8af64e39f02f2b82b37da347fe78c
Keshav Tiwari <keshavtiwari@keshavs-macbook-air.local>
2018-01-12 12:27:11 +0530

    Initial Commit

---
+++ ReadMoreButton-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReadMoreButton-iOS
+//
+//  Created by Keshav Tiwari on 12/01/18.
+//  Copyright © 2018 Keshav Tiwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
